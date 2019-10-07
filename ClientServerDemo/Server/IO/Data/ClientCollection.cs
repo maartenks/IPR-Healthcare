e9b952432c773e3e8740f77362dfb4799f31f87e
Joëlle <joelle.a.m.devries@gmail.com>
2019-10-07 13:00:47 +0200

    IO

---
+++ ClientServerDemo/Server/IO/Data/ClientCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Server.IO.Data
+{
+    public class ClientCollection
+    {
+        public Client[] clients { get; set; }
+
+        internal object OfType<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
